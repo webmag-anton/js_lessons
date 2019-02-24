049ca08c3e246db667686d346918fb46a75cb8ed
Anton Byshok <lyooon92@gmail.com>
2019-02-24 20:42:24 +0200

    bankomat

--- script_6.js
+++ script_6.js
@@ -173,23 +173,31 @@
 
 
 
-function sum(a) {
+// function sum(a) {
 
-  var currentSum = a;
+//   var currentSum = a;
 
-  function f(b) {
-    currentSum += b;
-    return f; // функция не вызывает сама себя (нет рекурсивного вызова), а возвращает ссылку на себя
-  }
+//   function f(b) {
+//     currentSum += b;
+//     return f; // функция не вызывает сама себя (нет рекурсивного вызова), а возвращает ссылку на себя
+//   }
 
-  f.toString = function() {
-    return currentSum; // происходит при строковом преобразовании - например alert(obj)  
-  };
+//   f.toString = function() {
+//     return currentSum; // происходит при строковом преобразовании - например alert(obj)  
+//   };
 
-  return f;
-}
+//   return f;
+// }
 
-alert( sum(1)(2) ); // 3
-alert( sum(5)(-1)(2) ); // 6
-alert( sum(6)(-1)(-2)(-3) ); // 0
-alert( sum(0)(1)(2)(3)(4)(5) ); // 15
+// alert( sum(1)(2) ); // 3
+// alert( sum(5)(-1)(2) ); // 6
+// alert( sum(6)(-1)(-2)(-3) ); // 0
+// alert( sum(0)(1)(2)(3)(4)(5) ); // 15
+
+
+
+
+
+
+
+
