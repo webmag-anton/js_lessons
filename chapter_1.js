7f2ee32bcb2a7181478f2dabc55094c71babf1fc
Anton Byshok <lyooon92@gmail.com>
2019-03-02 18:56:31 +0200

    chapter_1

---
+++ chapter_1.js
@@ -0,0 +1,26 @@
+// alert(null == null);  //true
+// alert(undefined == undefined);  //true
+// alert(null == undefined);  //true
+
+
+
+
+// var login = prompt('Ваш логин','Админ');
+
+// if (login == 'Админ') {
+
+// 	var password = prompt('Ваш пароль','Чёрный Властелин')
+// 	if (password == 'Чёрный Властелин') {
+// 		alert('Добро пожаловать!');
+// 	} else if (password == null) {
+// 		alert('Вход отменён');
+// 	} else {
+// 		alert('Пароль неверен');
+// 	}
+
+// } else if (login == null) {
+// 	alert('Вход отменён');
+// }	else{
+// 	alert('Я вас не знаю');
+// }
+
