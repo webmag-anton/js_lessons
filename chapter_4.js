a9c16b9b0a8f4d639ee2fcd0b2a5a3e9a6a65510
Anton Byshok <lyooon92@gmail.com>
2019-03-04 22:26:45 +0200

    1

---
+++ chapter_4.js
@@ -0,0 +1,26 @@
+// Сложение цен
+// var price1 = 0.1;
+// var price2 = 0.2;
+// var sum =  +(price1 + price2).toFixed(2) ;
+// alert( sum );
+
+
+
+
+
+// Случайное из интервала (min, max)
+// var min = 5,
+//   	max = 10;
+// alert( min + Math.random() * (max - min) );
+
+
+
+
+// Случайное целое от min до max
+// function randomInteger(min, max) {
+//   var rand = min - 0.5 + Math.random() * (max - min + 1)
+//   rand = Math.round(rand);
+//   return rand;
+// }
+// alert( randomInteger(5, 10) );
+
