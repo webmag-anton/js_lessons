7b50d7711b71c25e5c362a54aab48719237dc170
Anton Byshok <lyooon92@gmail.com>
2019-03-20 21:10:59 +0200

    1

--- chapter_6.js
+++ chapter_6.js
@@ -358,3 +358,30 @@
 // vasya.lastName = 'Макаров';
 
 // alert( vasya.fullName ); // Василий Сидоров
+
+
+
+
+
+
+
+
+// function Article() {
+//   this.created = new Date();
+//   Article.count++;
+//   Article.lastDate = this.created;
+// }
+
+// Article.count = 0;
+// Article.showStats = function() {
+// 	alert( 'Создано ф-й: ' + this.count + '\n Последняя: ' + this.lastDate ); 
+// }
+
+// new Article();
+// new Article();
+
+// Article.showStats(); // Всего: 2, Последняя: (дата)
+
+// new Article();
+
+// Article.showStats(); // Всего: 3, Последняя: (дата)
