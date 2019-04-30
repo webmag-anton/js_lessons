c100fab0e5d98157f8b7ce85a70980646617e940
Anton Byshok <lyooon92@gmail.com>
2019-04-30 12:52:43 +0300

    chapter 7

--- chapter_7.js
+++ chapter_7.js
@@ -399,3 +399,139 @@
 
 // f1000("тест"); // выведет "тест" через 1000 миллисекунд
 // f1500("тест2"); // выведет "тест2" через 1500 миллисекунд
+
+
+
+
+
+
+
+			// 7.5 Перехват ошибок, "try..catch"
+
+
+// // Т.к нет св-ва name, то ошибка(SyntaxError) бросается в catch, а там идет 
+// // проверка - если имя ошибки SyntaxError, то обрабатывается, а если другой 
+// // вид ошибки - то выбрасыватся наружу (в консоль или «более внешний» try..catch )  
+
+// var data = '{"age": 30 }'; // данные корректны
+
+// try {
+
+//   var user = JSON.parse(data);
+
+//   if (!user.name) {
+//     throw new SyntaxError("Ошибка в данных!!!");
+//   }
+
+//   blabla(); // произошла 
+
+//   alert( user.name );
+
+// } catch (e) {
+
+//   if (e.name == "SyntaxError") {
+//     alert( 	e.message );
+//   } else {
+//     throw e;
+//   }
+
+// }
+
+
+
+
+
+
+
+
+// function readData() {
+//   var data = '{ "name": "Вася", "age": 30 }';
+
+//   try {
+//     // ...
+//     blabla(); // ошибка!
+//   } catch (e) {
+//     // ...
+//     if (e.name != 'SyntaxError') {
+//       throw e; // пробрасываем
+//     }
+//   }
+// }
+
+// try {
+//   readData();
+// } catch (e) {
+//   alert( "Поймал во внешнем catch: " + e ); // ловим
+// }
+
+
+
+
+
+
+
+
+
+// function ReadError(message, cause) {
+//   this.message = message;
+//   this.cause = cause;
+//   this.name = 'ReadError';
+//   this.stack = cause.stack;
+// }
+
+// function readData() {
+//   var data = '{ bad data }';
+
+//   try {
+//     // ...
+//     JSON.parse(data);
+//     // ...
+//   } catch (e) {
+//     // ...
+//     if (e.name == 'URIError') {
+//       throw new ReadError("Ошибка в URI", e);
+//     } else if (e.name == 'SyntaxError') {
+//       throw new ReadError("Синтаксическая ошибка в данных", e);
+//     } else {
+//       throw e; // пробрасываем
+//     }
+//   }
+// }
+
+// try {
+//   readData();
+// } catch (e) {
+//   if (e.name == 'ReadError') {
+//     alert( e.message );
+//     alert( e.cause ); // оригинальная ошибка-причина
+//   } else {
+//     throw e;
+//   }
+// }
+
+
+
+
+
+
+
+
+
+// function sum(n) {
+//   return n ? (n + sum(n - 1)) : 0;
+// }
+
+// var n = +prompt('Введите n?', 100);
+
+// var start = new Date();
+
+// try {
+//   var result = sum(n);
+// } catch (e) {
+//   result = 0;
+// } finally {
+//   var diff = new Date() - start;
+// }
+
+// alert( result ? result : 'была ошибка' );
+// alert( "Выполнение заняло " + diff );
