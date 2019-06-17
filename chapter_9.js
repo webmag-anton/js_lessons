f01d04c7a8717abc6426470115cb81c161590804
Anton Byshok <lyooon92@gmail.com>
2019-06-17 23:23:23 +0300

    9.8

--- chapter_9.js
+++ chapter_9.js
@@ -687,3 +687,131 @@
 //   menu.close();
 //   menu.showState(); // закрыто
 // }, 1000);
+
+
+
+
+
+
+
+
+
+		// 9.7  Свои ошибки, наследование от Error
+
+
+
+
+// // задача
+
+// function FormatError(message) {
+//   this.name = "FormatError";
+
+//   this.message = message;
+
+//   if (Error.captureStackTrace) {
+//     Error.captureStackTrace(this, this.constructor);   /* для V8 (Chrome, Opera, Node.JS) */
+//   } else {
+//     this.stack = (new Error()).stack;									 /* для остального */		
+//   }
+
+// }
+
+// FormatError.prototype = Object.create(SyntaxError.prototype);
+// FormatError.prototype.constructor = FormatError;
+
+// // Использование
+
+// var err = new FormatError("ошибка форматирования");
+
+// alert( err.message ); // ошибка форматирования
+// alert( err.name ); // FormatError
+// alert( err.stack ); // стек на момент генерации ошибки
+
+// alert( err instanceof SyntaxError ); // true
+
+
+
+
+
+
+
+
+
+
+		// 9.8  ПРимеси ( mixins )
+
+
+
+
+// // примесь
+// var sayHiMixin = {
+//   sayHi: function() {
+//     alert("Привет " + this.name);
+//   },
+//   sayBye: function() {
+//     alert("Пока " + this.name);
+//   }
+// };
+
+// // использование:
+// function User(name) {
+//   this.name = name;
+// }
+
+// // передать методы примеси
+// for(var key in sayHiMixin) User.prototype[key] = sayHiMixin[key];
+
+// // User "умеет" sayHi
+// new User("Вася").sayHi(); // Привет Вася
+
+
+
+
+
+// var eventMixin = {
+
+//   /**
+//    * Подписка на событие
+//    * Использование:
+//    *  menu.on('select', function(item) { ... }
+//    */
+//   on: function(eventName, handler) {
+//     if (!this._eventHandlers) this._eventHandlers = {};
+//     if (!this._eventHandlers[eventName]) {
+//       this._eventHandlers[eventName] = [];
+//     }
+//     this._eventHandlers[eventName].push(handler);
+//   },
+
+//   /**
+//    * Прекращение подписки
+//    *  menu.off('select',  handler)
+//    */
+//   off: function(eventName, handler) {
+//     var handlers = this._eventHandlers && this._eventHandlers[eventName];
+//     if (!handlers) return;
+//     for(var i=0; i<handlers.length; i++) {
+//       if (handlers[i] == handler) {
+//         handlers.splice(i--, 1);
+//       }
+//     }
+//   },
+
+//   /**
+//    * Генерация события с передачей данных
+//    *  this.trigger('select', item);
+//    */
+//   trigger: function(eventName /*, ... */) {
+
+//     if (!this._eventHandlers || !this._eventHandlers[eventName]) {
+//       return; // обработчиков для события нет
+//     }
+
+//     // вызвать обработчики
+//     var handlers = this._eventHandlers[eventName];
+//     for (var i = 0; i < handlers.length; i++) {
+//       handlers[i].apply(this, [].slice.call(arguments, 1));
+//     }
+
+//   }
+// };
