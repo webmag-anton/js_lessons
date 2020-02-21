324a12aa43379734a6cb4fab0f9a8ba1140fcca5
anton <lyooon92@gmail.com>
2020-02-21 17:53:30 +0200

    1

--- synopsis/playground__3.js
+++ synopsis/playground__3.js
@@ -25,9 +25,9 @@
 
 
 
-// Пример XMLHttpRequest:
+	// Пример XMLHttpRequest:
 
-// const requestURL = 'https://jsonplaceholder.typicode.com/userse'
+// const requestURL = 'https://jsonplaceholder.typicode.com/users'
 
 // function sendRequest(method, url, body = null) {
 // 	return new Promise( (resolve, reject) => {
@@ -52,9 +52,9 @@
 // 	})
 // }
 
-// // sendRequest('GET', requestURL)
-// // 	.then(data => console.log(data))
-// // 	.catch(err => console.log(err))
+// sendRequest('GET', requestURL)
+// 	.then(data => console.log(data))
+// 	.catch(err => console.log(err))
 
 // const body = {
 // 	name: 'anton', 
@@ -67,63 +67,129 @@
 
 
 
-// Пример fetch (GET):
+	// Пример fetch (GET):
 
-const requestFetchURL = 'https://jsonplaceholder.typicode.com/todos'
+// const requestFetchURL = 'https://jsonplaceholder.typicode.com/todos'
 
-async function sendFetchGET(url) {
-	try {
-		let response = await fetch(url) // (await возвращает результат промиса; fetch() возвращает промис)
+// async function sendFetchGET(url) {
+// 	try {
+// 		let response = await fetch(url) // (await возвращает результат промиса; fetch() возвращает промис)
 
-		if (response.ok) { // если HTTP-статус в диапазоне 200-299
-		  let json = await response.json()  // получаем тело ответа (await возвращает результат промиса; json() возвращает промис)
-		  console.log(json)
-		} else {
-			console.error(`Ошибка HTTP: статус ${response.status}`)
-		}
-	} 
-	catch(err) {
-		console.error(`ошибка при запросе, возможно невалидный url!!!! ловим ошибку, выброшенную из fetch(). Объект ошибки - ${err}`)
-	}
-}
+// 		if (response.ok) { // если HTTP-статус в диапазоне 200-299
+// 		  let json = await response.json()  // получаем тело ответа (await возвращает результат промиса; json() возвращает промис)
+// 		  console.log(json)
+// 		} else {
+// 			console.error(`Ошибка HTTP: статус ${response.status}`)
+// 		}
+// 	} 
+// 	catch(err) {
+// 		console.error(`ошибка при запросе, возможно невалидный url!!!! ловим ошибку, выброшенную из fetch(). Объект ошибки - ${err}`)
+// 	}
+// }
 
-sendFetchGET(requestFetchURL)
+// sendFetchGET(requestFetchURL)
 
 
 
-// пример fetch (POST):
+	// пример fetch (POST):
 
-const requestFetchURL_POST = 'https://jsonplaceholder.typicode.com/albumsq' // albums
+// const requestFetchURL_POST = 'https://jsonplaceholder.typicode.com/albumsq' // albums
 
-function sendFetchPOST(method, url, body = null) {
-	// так как тело запроса body – строка, то заголовок Content-Type по умолчанию будет text/plain
-	const headers = {
-		'Content-Type': 'application/json'
-	}
+// function sendFetchPOST(method, url, body = null) {
+// 	// так как тело запроса body – строка, то заголовок Content-Type по умолчанию будет text/plain
+// 	const headers = {
+// 		'Content-Type': 'application/json'
+// 	}
 
-	return fetch(url, {
-		method: method,
-		body: JSON.stringify(body),
-		headers: headers
-	})
-		.then( response => {
-			if (response.ok) {
-				return response.json()
-			}
+// 	return fetch(url, {
+// 		method: method,
+// 		body: JSON.stringify(body),
+// 		headers: headers
+// 	})
+// 		.then( response => {
+// 			if (response.ok) {
+// 				return response.json()
+// 			}
 
-			// если код HTTP-статуса не в диапазоне 200-299
-			return response.json().then( error => {
-				const e = new Error(`Что то пошло не так...  ошибка ${response.status}`)
-				e.data = error
-				throw e
-			})
-		})
-}
+// 			// если код HTTP-статуса не в диапазоне 200-299
+// 			return response.json().then( error => {
+// 				const e = new Error(`Что то пошло не так...  ошибка ${response.status}`)
+// 				e.data = error
+// 				throw e
+// 			})
+// 		})
+// }
 
-const body = {
-	name: 'Vladilen', 
-	age: 26
-}
-sendFetchPOST('POST', requestFetchURL_POST, body)
-	.then(data => console.log(data))
-	.catch(err => console.log(err)) // отловим ошибку запроса fetch(), например если задать url https://jsonplacehol
+// const body = {
+// 	name: 'Vladilen', 
+// 	age: 26
+// }
+// sendFetchPOST('POST', requestFetchURL_POST, body)
+// 	.then(data => console.log(data))
+// 	.catch(err => console.log(err)) // отловим ошибку запроса fetch(), например если url https://jsonplac, а так же если !response.ok
+
+
+
+/* 3.1  Fetch */
+
+
+// // менее подходящий вариант, т.к. промисы ждут друг друга (await) + не сохраняется очередность в массиве ответов
+
+// function getUsers(names) {
+// 	const users = []
+
+// 	names.forEach( async function(name) {
+// 		try{
+// 			let response = await fetch(`https://api.githuB.com/users/${name}`) 
+
+// 			if (response.ok) {
+// 				let user = await response.json()
+// 				users.push(user)
+// 			} else {
+// 				users.push(null)
+// 				console.error(`пользователя github с именем ${name} не существует !!`)
+// 			}
+// 		}
+// 		catch(err) {
+// 			users.push(null)
+// 			console.error(`Невалидный url запроса fetch(); Объект ошибки - ${err}`)
+// 		}
+// 	})
+
+// 	return users
+// }
+
+// let usersObject = getUsers(['webmag-anton', 'some-does-not-exist', 'qwerty'])
+// console.log(usersObject)
+
+
+
+// // Более правильный выриант, т.к. промисы не ждут друг друга; в то же время Promise.all() сохраняет последовательность вызовов
+
+// async function getUsers(names) {
+//   let jobs = []
+
+//   for(let name of names) {
+//     let job = fetch(`https://api.github.com/users/${name}`).then(
+//       successResponse => {
+//         if (successResponse.status != 200) {
+//           return null;
+//         } else {
+//           return successResponse.json()
+//         }
+//       },
+//       failResponse => {
+//         return null
+//       }
+//     )
+//     // пушим асинхронные обработчики then, которые выполнятся ( successResponse.json() ) в том же порядке благодаря Promise.all
+//     jobs.push(job);
+//   }
+
+//   let results = await Promise.all(jobs)
+
+//   return results
+// }
+
+// let usersObject = getUsers(['webmag-anton', 'some-does-not-exist', 'qwerty'])
+// console.log(usersObject)
