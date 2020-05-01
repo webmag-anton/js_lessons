077da1333692531d067b6bb02e56280238dcd2b2
Anton <lyooon92@gmail.com>
2020-05-01 16:44:04 +0300

    1

---
+++ synopsis/javascript__tricks.js
@@ -0,0 +1,48 @@
+
+                                    /***  TRICKS  ***/
+
+      // NUMBERS
+
+// способ устраняющий неточность при округлении
+num.toFixed(n) – <> - округляет число до n знаков после запятой,
+                            допускает неточность! ( 6.35.toFixed(1) == 6.3 )
+Math.round(6.35 * 10) / 10 // 6.35 -> 63.5 -> 64(rounded) -> 6.4
+
+// быстрое возведение в степень
+console.log(2 ** 3); // Result: 8
+
+
+
+      // ARRAYS
+
+arr.length = 0  – самый простой способ очистить массив  
+[1, 2, 3, 4, 5].length = 3 // превращает в  [1, 2, 3]
+
+// делаем массив с уникальными значениями
+const array = [1, 1, 2, 3, 5, 5, 1]
+const uniqueArray = [...new Set(array)]  // [1, 2, 3, 5]
+
+// делаем из массива объект
+const users = [1, 2, 3, 4, 5]
+const usersObject = {...users}  // { '0':1, '1':2, '2':3, '3':4, '4':5 }
+
+
+
+      // Cloning
+
+   // not-deep object
+
+- Object.assign(dest, [src1, src2...]) // копирует св-ва {и строковые и символьные} всех объектов srcN в dest
+// точная копия obj c тем же прототипом - «продвинутое» клонирование объекта 
+- let clone = Object.create(Object.getPrototypeOf(obj), Object.getOwnPropertyDescriptors(obj))
+- let obj2 = {...obj1}
+
+   // not-deep array
+
+- arr.concat()
+- let arr2 = [...arr1]
+
+   // deep
+
+// этот прием нельзя использовать для копирования методов объекта (JSON не поддерживает функции)
+- let clone = JSON.parse(JSON.stringify(obj/arr)) 
